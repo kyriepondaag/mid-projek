ace7f8da0cfeec2fc2fda358f55f7844be097675
unknown <s2200045@student.unklab.ac.id>
2022-06-30 22:08:05 +0800

    slice

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,109 +1,21 @@
 import "../assets/style.css"
 import Nav from "../molekul/Nav";
 import Biodata from "../molekul/Biodata";
+import BiodataContent from "../molekul/BiodataContent";
+import RiwayatPendidikan from "../molekul/RiwayatPendidikan";
+import RiwayatPendidikanContent from "../molekul/RiwayatPendidikanContent";
+import RiwayatPekerjaan from "../molekul/RiwayatPekerjaan";
+import RiwayatPekerjaanContent from "../molekul/RiwayatPekerjaanContent";
 export default function Homepage() {
     return(
 <div>
   <Nav/>
   <Biodata/>
-  <div className="content">
-    <table>
-      <tbody><tr> 
-          <td> Nama</td>
-          <td>: CHRISTIAN SOLAFIDE HAERANI</td>
-        </tr>
-        <tr>
-          <td> NIM</td>
-          <td>: 105022010008</td>  
-        </tr>
-        <tr> 
-          <td> Status</td>
-          <td>: Mahasiswa</td>
-        </tr>
-        <tr> 
-          <td> Tempat, Tanggal Lahir</td>
-          <td>: Jakarta, 17 Januari 2002</td>
-        </tr>
-        <tr> 
-          <td> Agama</td>
-          <td>: Kristen</td>
-        </tr>
-        <tr> 
-          <td> Jenis Kelamin</td>
-          <td>: Laki-Laki</td>
-        </tr>
-        <tr> 
-          <td> Alamat</td>
-          <td>: Bitung Tengah</td>
-        </tr>
-        <tr> 
-          <td> Hobby</td>
-          <td>: Tenis Meja</td>
-        </tr>
-        <tr> 
-          <td> Golongan Darah</td>
-          <td>: A</td>
-        </tr>
-        <tr> 
-          <td> Email</td>
-          <td>: haeranic007@gmail.com</td>
-        </tr>
-        <tr> 
-          <td> Fakultas/Jurusan</td>
-          <td>: Fik/informatika</td>
-        </tr>
-        <tr className="hide">
-          <td> Tempat, Tanggal Lahir</td>
-          <td>: Menjabat sebagai MD di PT.Indomarco Prismatama</td>
-        </tr>
-      </tbody></table>
-  </div>
-  <div id="riwayatPendidikan">
-    <h1>RIWAYAT PENDIDIKAN</h1>
-  </div>
-  <div className="content">
-    <table>
-      <tbody><tr>
-          <td>2006-2007</td>
-          <td>: Tk GMIM Nasaret Bitung</td>
-        </tr>
-        <tr>
-          <td>2007-20013</td>
-          <td>: SD Katolik 3 DonBosco Bitung</td>
-        </tr>
-        <tr>
-          <td>20013-20016</td>
-          <td>: SMP Kristen Bitung</td>
-        </tr>
-        <tr>
-          <td>20016-20019</td>
-          <td>: SMA Katolik DonBosco Bitung</td>
-        </tr>
-        <tr>
-          <td>2020-sekarang</td>
-          <td>: Perguruan Tinggi Universitas Klabat</td>
-        </tr>
-        <tr className="hide">
-          <td> Tempat, Tanggal Lahir</td>
-          <td>: Menjabat sebagai MD di PT.Indomarco Prismatama</td>
-        </tr>
-      </tbody></table>
-  </div>
-  <div id="riwayatPekerjaan">
-    <h1>RIWAYAT PEKERJAAN</h1>
-  </div>
-  <div className="content">
-    <table>
-      <tbody><tr>
-          <td>2019-2020</td>
-          <td>: Menjabat sebagai MD di PT.Indomarco Prismatama</td>
-        </tr>
-        <tr className="hide">
-          <td> Tempat, Tanggal Lahir</td>
-          <td>: Menjabat sebagai MD di PT.Indomarco Prismatama</td>
-        </tr>
-      </tbody></table>
-  </div>
+  <BiodataContent/>
+  <RiwayatPendidikan/>
+  <RiwayatPendidikanContent/>
+  <RiwayatPekerjaan/>
+  <RiwayatPekerjaanContent/>
   <div id="contact">
     <h1>CONTACT PERSON</h1>
     <div className="contact">
